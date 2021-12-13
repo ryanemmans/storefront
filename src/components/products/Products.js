0e75aab9226eaf1537b6679235392a27047f6b14
Ryan Emmans <ryanemmans@gmail.com>
2021-12-12 22:04:19 -0800

    Updates styling and layout, updates readme.

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,46 +1,48 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Grid, Typography, Card, CardActions, CardContent, CardMedia, Button } from '@mui/material';
+import { Container, Grid, Typography, Card, CardActions, CardContent, CardMedia, Button } from '@mui/material';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
-// import './products.scss';
+import './products.css';
 
 function Products(props) {
   console.log(props);
 
   return (
-    <Grid container columns={{ xs: 4, sm: 8, md: 12 }} direction="row">
-      {props.products.length === 0 && (<Typography>No products yet.</Typography>)}
-      {props.products.map((product, idx) => {
-        return (
-          <Grid container item xs={4} sm={4} md={4} justifyContent="center">
-            <Card style={{ margin: '1rem' }} sx={{ width: 350 }} data-testid={`product-${product.name}`}>
-              <CardMedia
-                component="img"
-                height="200"
-                image={product.img}
-                alt={product.name}
-              />
-              <CardContent>
-                <Typography variant="h6" component="div" data-testid={`product-name-${product.name}`}>
-                  {product.name}
-                </Typography>
-                <Typography color="text.secondary" variant="body2" data-testid={`product-description-${product.name}`}>
-                  {product.description}
-                </Typography>
-                <Typography variant="caption">
-                  <strong>${product.price}</strong> ( {product.count} in stock )
-                </Typography>
-              </CardContent>
-              <CardActions style={{ display: 'flex', justifyContent: 'center', marginTop: '-1.5rem' }}>
-                <Button color="error" startIcon={<AddShoppingCartIcon />}>Add to Cart</Button>
-                <Button color="error" startIcon={<InfoOutlinedIcon />}>View Details</Button>
-              </CardActions>
-            </Card>
-          </Grid>
-        );
-      })}
-    </Grid>
+    <Container id='productContainer' maxWidth="lg" data-testid="products">
+      <Grid container spacing={4}>
+        {props.products.length === 0 && (<Typography>No products yet.</Typography>)}
+        {props.products.map((product, idx) => {
+          return (
+            <Grid item key={product.name} xs={12} sm={6} md={4}>
+              <Card data-testid={`product-${product.name}`}>
+                <CardMedia
+                  component="img"
+                  height="200"
+                  image={product.img}
+                  alt={product.name}
+                />
+                <CardContent>
+                  <Typography variant="h6" component="div" data-testid={`product-name-${product.name}`}>
+                    {product.name}
+                  </Typography>
+                  <Typography color="text.secondary" variant="body2" data-testid={`product-description-${product.name}`}>
+                    {product.description}
+                  </Typography>
+                  <Typography variant="caption">
+                    <strong>${product.price}</strong> ( {product.count} in stock )
+                  </Typography>
+                </CardContent>
+                <CardActions style={{ display: 'flex', justifyContent: 'center', marginTop: '-1.5rem' }}>
+                  <Button color="error" startIcon={<AddShoppingCartIcon />}>Add to Cart</Button>
+                  <Button color="error" startIcon={<InfoOutlinedIcon />}>View Details</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Container>
   );
 }
 
